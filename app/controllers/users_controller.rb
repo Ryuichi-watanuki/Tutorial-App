b969e843d4839a5652adbb4db601b9149118165f
Nukky <nukky874@icloud.com>
2019-02-18 08:19:40 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "ユーザーの新規作成に成功しました。"
       redirect_to @user
     else
@@ -18,10 +19,13 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  
 end
